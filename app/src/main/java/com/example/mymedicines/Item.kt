227a100693e6dbb985c2d6de1baffffff3f18f45
Alexey Gvozdarev <lelikfreeride@gmail.com>
2025-07-01 14:52:09 +0700

    - поправил метод isEven() в Item

--- app/src/main/java/com/example/mymedicines/Item.kt
+++ app/src/main/java/com/example/mymedicines/Item.kt
@@ -3,10 +3,15 @@ package com.example.mymedicines
 data class Item(
     val value: String,
     val number: Int? = null
-){
+) {
     override fun toString(): String {
         return if (number != null) "$value $number" else "$value"
     }
-    fun isEven(number: Int): Boolean = number % 2 != 0
+
+    fun isEven(): Boolean {
+        if (number == null) return false
+        return number % 2 == 0
+    }
+
 
 }
